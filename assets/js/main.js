ca5ef496421b9d880c127e4f7098e9008f96baaa
HeberRios <68972507+HeberRios@users.noreply.github.com>
2023-08-21 12:17:06 -0600

    Create a 16x16 grid with hover effect
    Was created a container for the squares, with a 16x16 grid template.
    Also in the main.js file added the "hover" effect with the mouseover
    event and the pixel trail with the mouseout event.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,28 @@
 "use strict";
+
+// SELECTING THE GRID CONTAINER
+const gridContainer = document.getElementById("main-grid-container");
+
+// CREATING THE DIVS WITHIN THE CONTAINER
+for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    gridContainer.appendChild(div);
+}
+
+// ADDING THE "HOVER" EVENT TO EACH DIV
+const squares = document.querySelectorAll(".square");
+
+// MOUSEOVER
+squares.forEach((square) => {
+    square.addEventListener("mouseover", function () {
+        square.style.backgroundColor = "#3A55CF";
+    });
+});
+
+// MOUSEOUT
+squares.forEach((square) => {
+    square.addEventListener("mouseout", function () {
+        square.style.backgroundColor = "#DB2B39";
+    });
+});
